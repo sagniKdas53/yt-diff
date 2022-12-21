18bf1daf35d78539bb94b98caa3cd7562988fcba
Sagnik Das <mailsagnikdas53@gmail.com>
2022-12-21 19:48:50 +0530

    will try to add the node into base os container

---
+++ db_test.js
@@ -0,0 +1,44 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('vidlist', 'ytdiff', 'ytd1ff', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+try {
+    sequelize.authenticate().then(
+        () => { console.log('Connection has been established successfully.'); })
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+const vid_list = sequelize.define('vid_list', {
+    // Model attributes are defined here
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    reference: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    downloaded: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+});
+
+sequelize.sync().then(() => {
+    console.log('vid_list table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
--- index.js
+++ index.js
@@ -3,7 +3,52 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 var qs = require('querystring');
+const { Sequelize, DataTypes } = require('sequelize');
+
 var port = process.argv[2] || 8888;
+const sequelize = new Sequelize('vidlist', 'ytdiff', 'ytd1ff', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+try {
+    sequelize.authenticate().then(() => {
+        console.log('Connection has been established successfully.');
+    })
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+const vid_list = sequelize.define('vid_list', {
+    // Model attributes are defined here
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    reference: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    downloaded: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+});
+
+sequelize.sync().then(() => {
+    console.log('vid_list table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
 
 var server = http.createServer((req, res) => {
     if (req.url === '/') {
