cb82dbad9891b13e1dfd70617b077c5dc4f1b9e5
Sagnik Das <mailsagnikdas53@gmail.com>
2022-12-22 15:58:22 +0530

    adding to database works now make it better

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ const vid_list = sequelize.define('vid_list', {
         allowNull: false,
         primaryKey: true
     },
-    reference: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
     title: {
         type: DataTypes.STRING,
         allowNull: false
@@ -41,7 +37,10 @@ const vid_list = sequelize.define('vid_list', {
     available: {
         type: DataTypes.BOOLEAN,
         allowNull: false
-    }
+    }, reference: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
 });
 
 sequelize.sync().then(() => {
@@ -57,7 +56,7 @@ var server = http.createServer((req, res) => {
         res.end();
     }
     else if (req.url === '/list' && req.method === 'POST') {
-        var body = '', body_url = '', response = '';
+        var body = '', body_url = '', response = '', end = '';
         req.on('data', function (data) {
             body += data;
             // Too much POST data, kill the connection!
@@ -72,35 +71,34 @@ var server = http.createServer((req, res) => {
             // as it seems these options are depricated `--playlist-start ${start_num}`, `--playlist-end ${stop_num}`, but still work
             console.log("url: ", body_url['url_input']);
             const yt_list = spawn("yt-dlp", ["--playlist-start", start_num, "--playlist-end", stop_num, "--flat-playlist",
-                "--print", '%(title)s \t %(id)s \t %(webpage_url)s', body_url['url_input']]);
-            yt_list.stdout.on("data", data => {
+                "--print", '%(title)s\t%(id)s\t%(webpage_url)s', body_url['url_input']]);
+            yt_list.stdout.on("data", async data => {
                 response += data;
-                data = '' + data;
-                console.log(data);
-                var items = data.split("\t");
-                console.log(items);
-                vid_list.create({
-                    url: items[2],
-                    reference: body_url['url_input'],
-                    title: items[0],
-                    downloaded: false,
-                    available: true
-                });
-                //return (`stdout: ${data}`);
             });
             yt_list.stderr.on("data", data => {
                 response = `stderr: ${data}`;
-                //return (`stderr: ${data}`);
             });
             yt_list.on('error', (error) => {
                 response = `error: ${error.message}`;
-                //return (`error: ${error.message}`);
             });
             yt_list.on("close", code => {
-                response += `child process exited with code ${code}`;
-                //console.log(`child process exited with code ${code}`);
+                end = `child process exited with code ${code}`;
+
+                response_list = response.slice(0, -2).split("\n");
+                console.log(response_list, response_list.length);
+                response_list.forEach(async element => { 
+                    var items = element.split("\t");
+                    console.log(items, items.length);
+                    const video = await vid_list.create({
+                        url: items[2],
+                        reference: body_url['url_input'],
+                        title: items[0],
+                        downloaded: false,
+                        available: true
+                    }).then(function () { console.log(items[0], 'saved'); });
+                });
                 res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(response);
+                res.end(response + end);
             });
         });
     }
