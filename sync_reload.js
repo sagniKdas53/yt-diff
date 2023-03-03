5b1b09d966ed6d0071a52fcaacdd3cd282a43252
Sagnik Das <mailsagnikdas53@gmail.com>
2023-03-03 16:39:45 +0530

    re-wrote most of it and fixed common errors

--- sync_reload.js
+++ sync_reload.js
@@ -5,12 +5,12 @@ const fs = require("fs");
 const { Sequelize, DataTypes, Op } = require("sequelize");
 const { Server } = require("socket.io");
 
-const protocol = process.env.protocol || 'http';
-const host = process.env.host || 'localhost';
+const protocol = process.env.protocol || "http";
+const host = process.env.host || "localhost";
 const port = process.env.port || 8888;
 const url_base = process.env.base_url || "/ytdiff";
 
-const db_host = process.env.db_host || 'localhost';
+const db_host = process.env.db_host || "localhost";
 const save_loc = process.env.save_loc || "yt-dlp";
 const sleep_time = process.env.sleep || 3;
 const subs_enabled = process.env.subs || true;
@@ -92,9 +92,9 @@ const play_lists = sequelize.define("play_lists", {
 });
 
 sequelize.sync().then(() => {
-    console.log('vid_list and play_lists tables exist or are created successfully!');
+    console.log("vid_list and play_lists tables exist or are created successfully!");
 }).catch((error) => {
-    console.error('Unable to create table : ', error);
+    console.error("Unable to create table : ", error);
 });
 
 async function download_lister(req, res) {
@@ -103,8 +103,8 @@ async function download_lister(req, res) {
         body += data;
         if (body.length > 1e6) {
             req.connection.destroy();
-            res.writeHead(413, { "Content-Type": html });
-            res.write("Content Too Large");
+            res.writeHead(413, { "Content-Type": json_t });
+            res.write({ "error": "Request Too Large" });
             res.end();
         }
     });
@@ -114,9 +114,9 @@ async function download_lister(req, res) {
         const response_list = { item: [] };
         for (const id_str of body["id"]) {
             const entry = await vid_list.findOne({ where: { id: id_str } });
-            response_list['item'].push([entry.url, entry.title]);
+            response_list["item"].push([entry.url, entry.title]);
         }
-        download_sequential(response_list['item']);
+        download_sequential(response_list["item"]);
         res.writeHead(200, { "Content-Type": json_t });
         res.end(JSON.stringify(response_list));
     });
@@ -126,7 +126,7 @@ async function download_sequential(items) {
     //console.log(items);
     for (const [url_str, title] of items) {
         try {
-            sock.emit('download-start', { message: title });
+            sock.emit("download-start", { message: title });
             const yt_dlp = spawn("yt-dlp", options.concat(url_str));
             yt_dlp.stdout.on("data", async (data) => {
                 try {
@@ -168,44 +168,43 @@ async function download_sequential(items) {
 // Add a parallel downloader someday
 
 async function list_init(req, res) {
-    var body = "",
-        init_resp = { count: 0, rows: [] };
+    var body = "";
     req.on("data", function (data) {
         body += data;
         if (body.length > 1e6) {
             req.connection.destroy();
-            res.writeHead(413, { "Content-Type": html });
-            res.write("Content Too Large");
+            res.writeHead(413, { "Content-Type": json_t });
+            res.write({ "error": "Request Too Large" });
             res.end();
         }
     });
     req.on("end", async function () {
         body = JSON.parse(body);
         //console.log("body_url: " + body["url"], "start_num: " + body["start"], "stop_num:", body["stop"]);
-        var body_url = body["url"];
-        var start_num = +body["start"] || 1;
-        var stop_num = +body["stop"] || 10;
-        var index = start_num - 1;
-        var chunk_size = +body["chunk"] || 10;
-        var watch_var = body["watch"] || false;
-        var full_update = body["full_update"] || false;
+        const start_num = +body["start"] || 1,
+            stop_num = +body["stop"] || 10,
+            chunk_size = +body["chunk"] || 10,
+            watch_it = body["watch"] || false,
+            full_update = body["full_update"] || false;
+        var body_url = body["url"],
+            index = start_num - 1;
         const response_list = await ytdlp_spawner(body_url, start_num, stop_num);
         //console.log(response_list, response_list.length);
         if (response_list.length > 1 || body_url.includes("playlist")) {
-            let title_str = "";
-            if (body_url.includes('youtube') && body_url.includes('/@')) {
+            var title_str = "";
+            if (body_url.includes("youtube") && body_url.includes("/@")) {
                 if (!/\/videos\/?$/.test(body_url)) {
-                    body_url = body_url.replace(/\/$/, '') + '/videos';
+                    body_url = body_url.replace(/\/$/, "") + "/videos";
                 }
                 //console.log(`${body_url} is a youtube channel`);
             }
-            if (body_url.includes('pornhub') && body_url.includes('/model/')) {
+            if (body_url.includes("pornhub") && body_url.includes("/model/")) {
                 if (!/\/videos\/?$/.test(body_url)) {
-                    body_url = body_url.replace(/\/$/, '') + '/videos';
+                    body_url = body_url.replace(/\/$/, "") + "/videos";
                 }
-                //console.log('Pornhub channel url: ' + body_url);
+                //console.log("Pornhub channel url: " + body_url);
             }
-            var is_alredy_indexed = await play_lists.findOne({
+            const is_alredy_indexed = await play_lists.findOne({
                 where: { url: body_url },
             });
             try {
@@ -237,7 +236,7 @@ async function list_init(req, res) {
                             where: { url: body_url },
                             defaults: {
                                 title: title_str,
-                                watch: watch_var,
+                                watch: watch_it,
                                 full_update: full_update
                             },
                         });
@@ -246,108 +245,46 @@ async function list_init(req, res) {
             }
         } else {
             body_url = "None";
+            const last_item = await vid_list.findOne({
+                where: {
+                    reference: "None",
+                },
+                order: [
+                    ["createdAt", "DESC"],
+                ],
+                attributes: ["list_order"],
+                limit: 1,
+            });
+            //console.log(last_item.list_order);
+            index = last_item.list_order;
         }
-
-        Promise.all(
-            response_list.map(async (element) => {
-                var items = element.split("\t");
-
+        processResponse(response_list, body_url, index)
+            .then(function (init_resp) {
                 try {
-                    var available_var = true;
-                    if (
-                        items[0] === "[Deleted video]" ||
-                        items[0] === "[Private video]"
-                    ) {
-                        available_var = false;
-                    }
-                    else if (items[0] === "NA") {
-                        items[0] = items[1];
-                    }
-                    if (body_url == "None") {
-                        const last_item = await vid_list.findOne({
-                            where: {
-                                reference: 'None',
-                            },
-                            order: [
-                                ['createdAt', 'DESC'],
-                            ],
-                            attributes: ['list_order'],
-                            limit: 1,
-                        });
-                        //console.log(last_item.list_order);
-                        index = last_item.list_order;
-                    }
-                    const [found, created] = await vid_list.findOrCreate({
-                        where: { url: items[2] },
-                        defaults: {
-                            id: items[1],
-                            reference: body_url,
-                            title: items[0],
-                            downloaded: false,
-                            available: available_var,
-                            list_order: ++index,
-                        },
-                    });
-                    if (!created) {
-                        // The object was found and not created
-                        //console.log("Found object: ", found);
-                        if (found.id !== items[1] ||
-                            found.reference !== body_url ||
-                            found.title !== items[0] ||
-                            found.available !== available_var ||
-                            found.list_order !== index - 1) {
-                            // At least one property is different, update the object
-                            found.id = items[1];
-                            found.reference = body_url;
-                            found.title = items[0];
-                            found.available = available_var;
-                            found.list_order = index - 1;
-                            //console.log("Found object updated: ", found);
-                        } else {
-                            found.changed("updatedAt", true);
-                        }
-                        await found.save();
-                    }
-                    // finally updating the object to send to frontend
-                    init_resp["count"] += 1;
-                    init_resp["rows"].push(found);
-
+                    res.writeHead(200, { "Content-Type": json_t });
+                    res.end(JSON.stringify(init_resp));
                 } catch (error) {
                     console.error(error);
                 }
-            })
-        ).then(function () {
-            try {
-                res.writeHead(200, { "Content-Type": json_t });
-                res.end(JSON.stringify(init_resp, null, 2));
-            } catch (error) {
-                console.error(error);
-            }
-        }).then(function () {
-            list_background(body_url, start_num, stop_num, chunk_size).then(
-                () => {
-                    //console.log("done processing playlist");
-                    sock.emit("playlist", { message: "done processing playlist or channel" });
-                }
-            );
-        });
+            }).then(function () {
+                list_background(body_url, start_num, stop_num, chunk_size).then(
+                    () => {
+                        //console.log("done processing playlist");
+                        sock.emit("playlist-done", { message: "done processing playlist or channel" });
+                    }
+                );
+            });
     });
 }
 
-function sleep(s) {
-    return new Promise((resolve) => setTimeout(resolve, s * 1000));
-}
-
 async function list_background(body_url, start_num, stop_num, chunk_size) {
-    //console.log('In list_background', "body_url", body_url, "start_num", start_num, "stop_num", stop_num, "chunk_size", chunk_size);
-    while (true && (body_url != 'None')) {
+    //console.log("In list_background", "body_url", body_url, "start_num", start_num, "stop_num", stop_num, "chunk_size", chunk_size);
+    while (true && (body_url != "None")) {
         start_num = start_num + chunk_size;
         stop_num = stop_num + chunk_size;
         // ideally we can set it to zero but that would get us rate limited by the services
-        // getting this form docker compose isn't a bad idea either
-        // I plan on using sockets to communicate that this is still working
+        await new Promise((resolve) => setTimeout(resolve, sleep_time * 1000));
         //console.log("In background lister", "Chunk:", chunk_size, "Start:", start_num, "Stop:", stop_num);
-        await sleep(sleep_time);
         const response = await ytdlp_spawner(body_url, start_num, stop_num);
         if (response.length === 0) {
             break;
@@ -388,6 +325,7 @@ function ytdlp_spawner(body_url, start_num, stop_num) {
 
 async function processResponse(response, body_url, start_num) {
     var index = start_num;
+    const init_resp = { count: 0, rows: [] }
     //console.log("In processResponse", "Start:", start_num);
     sock.emit("progress", { message: `Processing: ${body_url} from ${index}` });
     await Promise.all(response.map(async (element) => {
@@ -407,7 +345,7 @@ async function processResponse(response, body_url, start_num) {
                     title: title,
                     downloaded: false,
                     available: item_available,
-                    list_order: index++,
+                    list_order: ++index,
                 },
             });
             if (!created) {
@@ -430,11 +368,14 @@ async function processResponse(response, body_url, start_num) {
                 }
                 await found.save();
             }
+            init_resp['count']++;
+            init_resp['rows'].push(found)
         } catch (error) {
             console.error(error);
         }
     })
     );
+    return init_resp;
 }
 
 async function playlists_to_table(req, res) {
@@ -443,27 +384,19 @@ async function playlists_to_table(req, res) {
         body += data;
         if (body.length > 1e6) {
             req.connection.destroy();
-            res.writeHead(413, { "Content-Type": html });
-            res.write("Content Too Large");
+            res.writeHead(413, { "Content-Type": json_t });
+            res.write({ "error": "Request Too Large" });
             res.end();
         }
     });
     req.on("end", function () {
         body = JSON.parse(body);
-        var row = "order_added";
-        var type = "ASC";
-        var start_num = body["start"] || 0;
-        var stop_num = body["stop"] || 10;
-        var sort_with = body["sort"] || 1;
-        var order = body["order"] || 1;
-        if (order == 2) {
-            type = "DESC";
-        }
-        if (sort_with == 2) {
-            row = "createdAt";
-        } else if (sort_with == 3) {
-            row = "updatedAt";
-        }
+        const start_num = body["start"] || 0,
+            stop_num = body["stop"] || 10,
+            sort_with = body["sort"] || 1,
+            order = body["order"] || 1,
+            type = (order == 2) ? "DESC" : "ASC", // 0, 1 it will be ascending else descending
+            row = (sort_with == 2) ? "createdAt" : (sort_with == 3) ? "updatedAt" : "order_added";
         play_lists.findAndCountAll({
             limit: stop_num - start_num,
             offset: start_num,
@@ -481,21 +414,19 @@ async function sublist_to_table(req, res) {
         body += data;
         if (body.length > 1e6) {
             req.connection.destroy();
-            res.writeHead(413, { "Content-Type": html });
-            res.write("Content Too Large");
+            res.writeHead(413, { "Content-Type": json_t });
+            res.write({ "error": "Request Too Large" });
             res.end();
         }
     });
     req.on("end", function () {
         body = JSON.parse(body);
-        var body_url = body["url"];
-        var start_num = +body["start"] || 0;
-        var stop_num = +body["stop"] || 10;
-        var query_string = body["query"] || "";
-        var order = "list_order", type = "ASC";
-        // This is a rough solution to a bigger problem, need more looking into
-        // if (body_url == "None") { order = "updatedAt", type = "DESC"; }
-        //console.log(`body_url: ${body_url}\nquery_string: "${query_string}"\nstart_num: ${start_num}\nstop_num: ${stop_num}\n`);
+        const body_url = body["url"],
+            start_num = +body["start"] || 0,
+            stop_num = +body["stop"] || 10,
+            query_string = body["query"] || "",
+            order = "list_order", type = "ASC";
+        // Sorting not implemented for sub-lists yet
         try {
             if (query_string == "") {
                 vid_list.findAndCountAll({
@@ -536,24 +467,24 @@ const html = "text/html; charset=utf-8";
 const js = "text/javascript; charset=utf-8";
 const json_t = "text/json; charset=utf-8";
 const staticAssets = {
-    '': { obj: (__dirname + '/index.html'), type: html },
-    '/': { obj: (__dirname + '/index.html'), type: html },
-    '/dbi': { obj: (__dirname + '/dbi.html'), type: html },
-    '/assets/bootstrap.min.css': { obj: (__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css'), type: css },
-    '/assets/bootstrap.min.css.map': { obj: (__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css.map'), type: css },
-    '/assets/bootstrap.bundle.min.js': { obj: (__dirname + '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'), type: js },
-    '/assets/bootstrap.bundle.min.js.map': { obj: (__dirname + '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map'), type: js },
-    '/assets/favicon.ico': { obj: (__dirname + '/favicon.ico'), type: "image/x-icon" },
-    '/assets/socket.io.min.js': { obj: (__dirname + '/node_modules/socket.io/client-dist/socket.io.min.js'), type: js },
-    '/assets/socket.io.min.js.map': { obj: (__dirname + '/node_modules/socket.io/client-dist/socket.io.min.js.map'), type: js },
-    '/assets/nav.png': { obj: (__dirname + '/nav.png'), type: "image/png" },
-    '/assets/client.js': { obj: (__dirname + '/client.js'), type: js }
+    "": { obj: (__dirname + "/index.html"), type: html },
+    "/": { obj: (__dirname + "/index.html"), type: html },
+    "/dbi": { obj: (__dirname + "/dbi.html"), type: html },
+    "/assets/bootstrap.min.css": { obj: (__dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css"), type: css },
+    "/assets/bootstrap.min.css.map": { obj: (__dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css.map"), type: css },
+    "/assets/bootstrap.bundle.min.js": { obj: (__dirname + "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"), type: js },
+    "/assets/bootstrap.bundle.min.js.map": { obj: (__dirname + "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map"), type: js },
+    "/assets/favicon.ico": { obj: (__dirname + "/favicon.ico"), type: "image/x-icon" },
+    "/assets/socket.io.min.js": { obj: (__dirname + "/node_modules/socket.io/client-dist/socket.io.min.js"), type: js },
+    "/assets/socket.io.min.js.map": { obj: (__dirname + "/node_modules/socket.io/client-dist/socket.io.min.js.map"), type: js },
+    "/assets/nav.png": { obj: (__dirname + "/nav.png"), type: "image/png" },
+    "/assets/client.js": { obj: (__dirname + "/client.js"), type: js }
 };
 
 const server = http.createServer((req, res) => {
     if (req.url.startsWith(url_base) && req.method === "GET") {
         try {
-            var get = req.url.replace(url_base, '')
+            const get = req.url.replace(url_base, "")
             res.writeHead(200, { "Content-Type": staticAssets[get].type });
             res.write(fs.readFileSync(staticAssets[get].obj));
         } catch (error) {
