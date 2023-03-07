10b47d148805dcfa0961291d3bfcbc0e40b66e00
Sagnik Das <mailsagnikdas53@gmail.com>
2023-03-07 16:13:57 +0530

    much improved logging, now only need a library

--- sync_reload.js
+++ sync_reload.js
@@ -155,8 +155,8 @@ async function url_to_title(body_url) {
         return body_url
     }
 }
-function ytdlp_spawner(body_url, start_num, stop_num) {
-    console.log(`In spawner\nStart: ${start_num}\nStop: ${stop_num}\nUrl: ${body_url}`);
+function list_spawner(body_url, start_num, stop_num) {
+    console.log(`\nlist_spawner:\n\tStart: ${start_num}\n\tStop: ${stop_num}\n\tUrl: ${body_url}`);
     return new Promise((resolve, reject) => {
         const yt_list = spawn("yt-dlp", [
             "--playlist-start",
@@ -180,19 +180,21 @@ function ytdlp_spawner(body_url, start_num, stop_num) {
             console.error(`error: ${error.message}`);
         });
         yt_list.on("close", (code) => {
+            if (code !== 0) {
+                console.error(`yt-dlp returned code: ${code}`);
+            }
             resolve(response.split("\n").filter((line) => line.length > 0));
         });
     });
 }
 async function processResponse(response, body_url, index) {
-    console.log(`In processResponse\nStart: ${index}\nUrl: ${body_url}`);
+    console.log(`\nprocessResponse:\n\tIndex: ${index}\n\tUrl: ${body_url}`);
     const init_resp = { count: 0, rows: [] };
     sock.emit("progress", { message: `Processing: ${body_url} from ${index}` });
     await Promise.all(response.map(async (element) => {
         var title = element.split("\t")[0].trim(),
             item_available = true;
         const [id, url] = element.split("\t").slice(1);
-        console.log(title, id, url);
         if (title === "[Deleted video]" || title === "[Private video]" || title === "[Unavailable video]") {
             item_available = false;
         }
@@ -245,13 +247,13 @@ async function sleep(sleep_seconds = sleep_time) {
 
 // The scheduled updater
 async function scheduledUpdate() {
-    console.log("Scheduled update started at:", new Date().toISOString());
+    console.log(`\nScheduled update started at: ${new Date().toISOString()}`);
     const playlists = await play_lists.findAndCountAll({
         where: {
             watch: true
         }
     });
-    console.log("Updating", playlists["rows"].length, "playlists");
+    console.log(`\nUpdating ${playlists["rows"].length} playlists`);
     for (const playlist of playlists["rows"]) {
         var index = 0;
         const last_item = await vid_list.findOne({
@@ -265,17 +267,17 @@ async function scheduledUpdate() {
             limit: 1,
         });
         try {
-            console.log(`Playlist: ${playlist.title.trim()} being updated from index ${last_item.list_order}`);
+            console.log(`\nPlaylist: ${playlist.title.trim()} being updated from index ${last_item.list_order}`);
             index = last_item.list_order;
         } catch (error) {
             // do nothing
         }
         await sleep();
         await list_background(playlist.url, index, index + 10, 10);
-        console.log(`Done processing playlist ${playlist.url}`);
+        console.log(`\nDone processing playlist ${playlist.url}`);
     }
-    console.log("Scheduled update finished at:", new Date().toISOString());
-    console.log(`Next scheduled update on ${job.nextDates(1)}`);
+    console.log(`\nScheduled update finished at: ${new Date().toISOString()}`);
+    console.log(`\nNext scheduled update on ${job.nextDates(1)}`);
 }
 
 // Download functions
@@ -308,7 +310,7 @@ async function download_lister(req, res) {
 }
 // Add a parallel downloader someday
 async function download_sequential(items) {
-    console.log(`Downloading ${items.length} videos sequential`);
+    console.log(`\nDownloading ${items.length} videos sequentially`);
     for (const [url_str, title, save_dir] of items) {
         try {
             const save_path = path_fs.join(save_loc, save_dir);
@@ -337,7 +339,7 @@ async function download_sequential(items) {
                 console.error(`error: ${error.message}`);
             });
             yt_dlp.on("close", async (code) => {
-                if (code == 0) {
+                if (code === 0) {
                     const entity = await vid_list.findOne({ where: { url: url_str } });
                     entity.set({
                         downloaded: true,
@@ -348,7 +350,7 @@ async function download_sequential(items) {
             });
             // this holds the for loop, preventing the next iteration from happening
             await new Promise((resolve) => yt_dlp.on("close", resolve));
-            console.log(`Downloaded ${title} at location ${save_path}`)
+            console.log(`\nDownloaded ${title} at location ${save_path}`)
         } catch (error) {
             console.error(error);
         }
@@ -366,37 +368,35 @@ async function list_init(req, res) {
             watch = body["watch"] || false;
         var body_url = body["url"],
             index = (start_num > 0) ? start_num - 1 : 0; // index starts from 0 in this function
-        console.log("In list_init function:", "\nbody_url: " + body["url"], "\nstart_num: " + body["start"], "\nstop_num:",
-            body["stop"], "\nchunk_size:", body["chunk"], "\ncontinuous:", body["continuous"], "\nindex:", index);
+        console.log(`\nlist_init:\n\tbody_url: ${body["url"]}\n\tstart_num: ${body["start"]}\n\t` +
+            `stop_num: ${body["stop"]}\n\tchunk_size: ${body["chunk"]}\n\t` +
+            `continuous: ${body["continuous"]}\n\tindex: ${index}`);
         /*This is to prevent spamming of the spawn process, since each spawn will only return first 10 items
         to the frontend but will continue in the background, this can cause issues like list_order getting 
         messed up or listing not completing.
         It"s best to not use bulk listing for playlists and channels but say you have 50 tabs open and you just 
         copy the urls then you can just set them to be processed in this mode.*/
         if (continuous) await sleep();
-        const response_list = await ytdlp_spawner(body_url, start_num, stop_num);
-        console.log("response_list: " + response_list + "\nresponse_list.length: " + response_list.length);
+        const response_list = await list_spawner(body_url, start_num, stop_num);
+        console.log(`\nresponse_list:\t${JSON.stringify(response_list,null,2)}\n\tresponse_list.length: ${response_list.length}`);
         if (response_list.length > 1 || body_url.includes("playlist")) {
-            var title_str = "";
             if (body_url.includes("youtube") && body_url.includes("/@")) {
                 if (!/\/videos\/?$/.test(body_url)) {
                     body_url = body_url.replace(/\/$/, "") + "/videos";
                 }
-                console.log(`${body_url} is a youtube channel`);
+                console.log(`\n${body_url} is a youtube channel`);
             }
             if (body_url.includes("pornhub") && body_url.includes("/model/")) {
                 if (!/\/videos\/?$/.test(body_url)) {
                     body_url = body_url.replace(/\/$/, "") + "/videos";
                 }
-                console.log("Pornhub channel url: " + body_url);
+                console.log(`\n${body_url} is a hub channel`);
             }
             const is_already_indexed = await play_lists.findOne({
                 where: { url: body_url },
             });
             try {
-                is_already_indexed.changed("updatedAt", true);
-                await is_already_indexed.save();
-                title_str = is_already_indexed.title;
+                is_already_indexed.title.trim();
             } catch (error) {
                 console.error("playlist or channel not encountered earlier, saving in playlist");
                 // Its not an error, but the title extraction, 
@@ -436,7 +436,7 @@ async function list_init(req, res) {
             }).then(function () {
                 list_background(body_url, start_num, stop_num, chunk_size).then(
                     () => {
-                        console.log(`Done processing playlist: ${body_url}`);
+                        console.log(`\nDone processing playlist: ${body_url}`);
                         sock.emit("playlist-done", { message: "done processing playlist or channel" });
                     }
                 );
@@ -466,13 +466,13 @@ async function watch_list(req, res) {
     }
 }
 async function list_background(body_url, start_num, stop_num, chunk_size) {
-    console.log("In background lister", "\nURL:", body_url, "\nChunk:", chunk_size, "\nStart:", start_num, "\nStop:", stop_num);
+    console.log(`\nlist_background:\n\tURL: ${body_url}\n\tChunk: ${chunk_size}\n\tStart: ${start_num}\n\tStop: ${stop_num}`);
     while (true && (body_url != "None")) {
         start_num = start_num + chunk_size;
         stop_num = stop_num + chunk_size;
         // ideally we can set it to zero but that would get us rate limited by the services
         await sleep();
-        const response = await ytdlp_spawner(body_url, start_num, stop_num);
+        const response = await list_spawner(body_url, start_num, stop_num);
         if (response.length === 0) {
             break;
         }
@@ -493,24 +493,28 @@ async function add_playlist(url_var, watch_var) {
         title_str += data;
     });
     get_title.on("close", async (code) => {
-        if (title_str == "NA\n") {
-            try {
-                title_str = await url_to_title(url_var);
-            } catch (error) {
-                title_str = url_var;
-                console.error(error);
+        if (code === 0) {
+            if (title_str == "NA\n") {
+                try {
+                    title_str = await url_to_title(url_var);
+                } catch (error) {
+                    title_str = url_var;
+                    console.error(error);
+                }
             }
+            title_str = await string_slicer(title_str, MAX_LENGTH)
+            // no need to use found or create syntax here as this is only run the first time a playlist is made
+            play_lists.findOrCreate({
+                where: { url: url_var },
+                defaults: {
+                    title: title_str,
+                    watch: watch_var,
+                    save_dir: title_str
+                },
+            });
+        } else {
+            console.error("Playlist could not be created");
         }
-        title_str = await string_slicer(title_str, MAX_LENGTH)
-        // no need to use found or create syntax here as this is only run the first time a playlist is made
-        play_lists.findOrCreate({
-            where: { url: url_var },
-            defaults: {
-                title: title_str,
-                watch: watch_var,
-                save_dir: title_str
-            },
-        });
     });
 }
 
@@ -525,8 +529,8 @@ async function playlists_to_table(req, res) {
             query_string = body["query"] || "",
             type = (order == 2) ? "DESC" : "ASC", // 0, 1 it will be ascending else descending
             row = (sort_with == 2) ? "createdAt" : (sort_with == 3) ? "updatedAt" : "order_added";
-        console.log("In playlists_to_table", "\nStart: ", start_num, "\nStop: ", stop_num,
-            "\nOrder: ", order, "\nType: ", type, "\nQuery: ", query_string);
+        console.log(`\nplaylists_to_table:\n\tStart: ${start_num}\n\tStop: ${stop_num}\n\t` +
+            `Order: ${order}\n\tType: ${type}\n\tQuery: "${query_string}"`);
         if (query_string == "") {
             play_lists.findAndCountAll({
                 limit: stop_num - start_num,
@@ -566,8 +570,8 @@ async function sublist_to_table(req, res) {
             stop_num = +body["stop"] || 10,
             query_string = body["query"] || "",
             order = "list_order", type = "ASC";
-        console.log("In sublist_to_table", "\nStart: ", start_num, "\nStop: ", stop_num,
-            "\nOrder: ", order, "\nType: ", type, "\nQuery: ", query_string, "\nReference: ", body_url);
+        console.log(`\nsublist_to_table:\n\tStart: ${start_num}\n\tStop: ${stop_num}\n\t` +
+            `Order: ${order}\n\tType: ${type}\n\tQuery: "${query_string}"\n\tReference: ${body_url}`);
         // Sorting not implemented for sub-lists yet
         try {
             if (query_string == "") {
@@ -627,13 +631,28 @@ const staticAssets = {
     "/assets/nav.png": { file: path_fs.join(__dirname, "/nav.png"), type: "image/png" },
     "/assets/client.js": { file: path_fs.join(__dirname, "/client.js"), type: js }
 };
+const staticAssetsSync = {
+    "": { file: fs.readFileSync(path_fs.join(__dirname, "/index.html")), type: html },
+    "/": { file: fs.readFileSync(path_fs.join(__dirname, "/index.html")), type: html },
+    "/dbi": { file: fs.readFileSync(path_fs.join(__dirname, "/dbi.html")), type: html },
+    "/assets/bootstrap.min.css": { file: fs.readFileSync(path_fs.join(__dirname, "/node_modules/bootstrap/dist/css/bootstrap.min.css")), type: css },
+    "/assets/bootstrap.min.css.map": { file: fs.readFileSync(path_fs.join(__dirname, "/node_modules/bootstrap/dist/css/bootstrap.min.css.map")), type: css },
+    "/assets/bootstrap.bundle.min.js": { file: fs.readFileSync(path_fs.join(__dirname, "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js")), type: js },
+    "/assets/bootstrap.bundle.min.js.map": { file: fs.readFileSync(path_fs.join(__dirname, "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map")), type: js },
+    "/assets/favicon.ico": { file: fs.readFileSync(path_fs.join(__dirname, "/favicon.ico")), type: "image/x-icon" },
+    "/assets/socket.io.min.js": { file: fs.readFileSync(path_fs.join(__dirname, "/node_modules/socket.io/client-dist/socket.io.min.js")), type: js },
+    "/assets/socket.io.min.js.map": { file: fs.readFileSync(path_fs.join(__dirname, "/node_modules/socket.io/client-dist/socket.io.min.js.map")), type: js },
+    "/assets/nav.png": { file: fs.readFileSync(path_fs.join(__dirname, "/nav.png")), type: "image/png" },
+    "/assets/client.js": { file: fs.readFileSync(path_fs.join(__dirname, "/client.js")), type: js }
+};
+const reloadAsset = (asset, reload = false) => { return reload ? fs.readFileSync(staticAssets[asset].file) : staticAssetsSync[asset].file; };
 
 const server = http.createServer((req, res) => {
     if (req.url.startsWith(url_base) && req.method === "GET") {
         try {
             const get = req.url.replace(url_base, "")
             res.writeHead(200, { "Content-Type": staticAssets[get].type });
-            res.write(fs.readFileSync(staticAssets[get].file));
+            res.write(reloadAsset(get, true));
         } catch (error) {
             res.writeHead(404, { "Content-Type": html });
             res.write("Not Found");
