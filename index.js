901cbffb6157d984a7462387448bd24d4e55ae7c
Sagnik Das <mailsagnikdas53@gmail.com>
2023-04-30 00:41:28 +0530

    testing material ui

--- index.js
+++ index.js
@@ -891,6 +891,8 @@ const io = new Server(server, {
       "https://lenovo-ideapad-320-15ikb.tail9ece4.ts.net",
       "http://192.168.0.103:8888",
       "http://192.168.0.106:8888",
+      "http://192.168.0.103:5173",
+      "http://192.168.0.106:5173",
     ],
   },
 });
